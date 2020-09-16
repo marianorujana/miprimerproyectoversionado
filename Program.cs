73f107d91f8cf7b025b1ca30df4fa897279d2e10
Mariano Rujana <mariano.rujana@desarrollosnea.com>
2020-09-15 21:15:33 -0300

    Primer commit del proyecto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiPrimerProyectoVersionado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
