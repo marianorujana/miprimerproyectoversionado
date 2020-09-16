f789fa8f5750189298076684371d4089799d8efa
soniarolon08 <rolonsoniaselena@gmail.com>
2020-09-15 22:33:31 -0300

    Modificacion de Sonia

---
+++ Sonia.cs
@@ -0,0 +1,65 @@
+HEAD
+﻿using System;
+
+namespace MiPrimerProyectoVersionado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Celeste
+
+            //Maxi
+
+            //Guada
+                    
+            //Rodrigo
+
+            //Sonia
+
+            //Franco Q
+
+            //Fede
+
+            //Simon
+
+            //Franco S
+        }
+    }
+}
+
+﻿using System;
+
+namespace MiPrimerProyectoVersionado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Celeste
+            Console.WriteLine("Presente");
+
+            //Maxi
+
+            //Guada
+
+            //Rodrigo
+
+            //Sonia
+            Console.WriteLine("Presente");
+            //Franco Q
+
+            //Fede
+            Console.WriteLine("Presente");
+
+            //Simon
+
+            //Franco S
+        }
+    }
+}
+
