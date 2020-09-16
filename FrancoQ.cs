fa3799d75043f3b167f25e42fab273b0011c92c3
FrancoRQuiroz <francoquiroz78@gmail.com>
2020-09-15 22:40:46 -0300

    Presente Franco

---
+++ FrancoQ.cs
@@ -0,0 +1,33 @@
+
+﻿using System;
+
+namespace MiPrimerProyectoVersionado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Celeste
+
+            //Maxi
+
+            //Guada
+
+            //Rodrigo
+
+            //Sonia
+
+            //Franco Q
+            Console.WriteLine("Hello World!");
+            //Fede
+
+            //Simon
+
+            //Franco S
+        }
+    }
+}
+
+﻿
